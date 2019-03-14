71b295d7ba069c7f03bd40575333f141320085d1
hamida-mstafa <hamidamstafa@gmail.com>
2019-03-14 18:22:41 +0300

    save the models in the application

--- courtlist/models.py
+++ courtlist/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class lawyers(models.Model)
+    name = models.CharField(max_length=20,default='')
+    id = models.primarykey()
+class courts(models.Model)
+    Cname = models.CharField()
+    Cno = models.Foreignkey()
+
+class courthearings(models.Model):
+    judge = models.TextField()
+    start_time = models.DateTimeField()
+
+class courtcasses(models.Model)
+    position = models.()
+    type = models.Charfield()
+    hearing = models.Foreignkey(courthearings, related_name='cases')
+    case = models.Foreignkey(casses)
+
+class casses(models.Model)
+    casenumber = models.Integerfield()
+    pleintiff = models.TextField()
+    defendant = models.TextField()
